7e2720e7e45f760f9ddd7a350900c1f5b63e0d4f
Peeravit <peeravit42@gmail.com>
2023-09-26 14:13:27 +0700

    function

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,9 @@
 // Exercise #5 Min Function
 // Start coding here
+function findMinNumber(a,b){
+    let min=Math.min(a,b)
+    return(min)
+}
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
